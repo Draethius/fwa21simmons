3b102b35d6a1855c666fdf4e59eeb04d76a92cac
Michael Simmons <s532521@nwmissouri.edu>
2021-10-27 17:34:03 -0500

    New endpoint

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var createError = require('http-errors');
-var express = require('express');
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var myDataRouter = require('./routes/mydata');
 
 var app = express();
 
@@ -20,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/mydata', myDataRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
